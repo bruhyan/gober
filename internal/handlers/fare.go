525abd765da04d495e82e42e8dd6d5ddd55718f0
bryan koh <bryankoh@bryans-MacBook-Pro.local>
2025-02-14 23:23:22 +0800

    feat: fare post request

--- internal/handlers/fare.go
+++ internal/handlers/fare.go
@@ -1,6 +1,7 @@
 package fare
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,31 @@ type Fare struct {
 	Price float64 `json:"price"`
 }
 
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type FareRequest struct {
+	Start       Location `json:"start"`
+	Destination Location `json:"destination"`
+}
+
 func GetFare(c *gin.Context) {
+	var request FareRequest
+
+	err := c.ShouldBindJSON(&request)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	start := request.Start
+	dest := request.Destination
+
+	fmt.Println(start, dest)
+
 	var mockFare = Fare{Price: 21.90}
 	c.IndentedJSON(http.StatusOK, mockFare)
 }
